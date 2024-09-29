2788fa71b28442fce301b77dc4cd882a5173b66f
Tim K <38655780+Dokaking@users.noreply.github.com>
2024-09-29 09:13:43 +0200

    Create BugetPlanner.cpp

---
+++ BugetPlanner.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <array>
+#include <vector>
+
+#include "BudgetPlanner.h"
+
+int main ()
+{
+  std::vector<int> income;
+  std::vector<int> costs;
+  int sum;
+
+  std::cout << "What is your income?" << std::flush;
+  std::cin >> income.push_back();
+  std::cout << std::endl;
+  
+  std::cout << "What are your costs?" << std::flush;
+  std::cin >> cost.push_back();
+  std::cout << std::endl;
+
+  calcSum(income, cost);
+
+  std::cout << "The sum is: " << sum << std::endl;
+}
